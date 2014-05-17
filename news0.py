36fc1ec5ca8c5e365de6da06188d4ff7301746aa
youzipi <blake_12@qq.com>
2014-05-17 23:05:30 +0800

    add： 	显示数据库中的title，digest，body todo： 	text和html的转换 	QString和string冲突
    Signed-off-by: youzipi <blake_12@qq.com>

--- news0.py
+++ news0.py
@@ -9,6 +9,15 @@ from PyQt4.QtCore import pyqtSignature
 from PyQt4.QtSql import *
 
 from Ui_news0 import Ui_MainWindow
+from html2text import _DeHTMLParser, dehtml, txt2html
+from text2html import *
+#from SqlModel import Model
+
+
+#QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
+
+
+cid = 0    #全局变量cid
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     """
@@ -51,25 +60,78 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     @pyqtSignature("QModelIndex")
     def on_tableView_clicked(self, index):    #表格元素点击
         print ("clicked")
-        print (index.column(), index.row())
-    
-    @pyqtSignature("QModelIndex")
-    def on_tableView_doubleClicked(self, index):    #表格元素双击
+        
         item = QStandardItem()
         print type(item)
         row = index.row()
         column = index.column()
+        self.ui.tableView.selectRow(row)
         #print self.ui.tableView.item(row, column).text()
         #print Model.data(Model.index(row, column)).text()
         #print Model.index(row, 0).data().toInt()[0]
         #print self.ui.tableView.selectionModel().model().index(row, 0).data().toInt()[0]
-        print self.ui.tableView.model().index(row, 0).data().toInt()[0]
-        #print self.ui.tableView.model().index(row, 2).data().toString()[0]
-        #print self.ui.tableView.currentIndex().row()
-        cid = row
+        nid = self.ui.tableView.model().index(row, 0).data().toInt()[0]
+        print nid
+        self.search(nid)
 
-        #self.ui.titleedit.setPlainText(title)
-        self.ui.tableView.selectRow(row)
+    def search(self, nid):
+        q = QSqlQuery()
+        nid = bytes(nid)
+        selection = "SELECT cid,title,digest,body FROM t_news WHERE nid = "+nid+";"
+        print selection
+        q.exec_(selection)
+        print type(q)
+        if q.first():
+            cid = q.value(0).toInt()
+            title = q.value(1).toString()
+            digest = q.value(2).toString()
+            body = q.value(3).toString()
+            #body.replace('<p>', '')
+            #body.replace('</p>', r'')
+        print type(title)
+        #print title
+        
+        #return cid
+
+
+        self.ui.titleedit.setPlainText(title)
+        self.ui.digestedit.setPlainText(digest)
+        #self.ui.bodyedit.setPlainText(body)
+        #print html_to_text(body)
+        print text_to_html(str(body))
+       
+        #print s
+
+        #self.ui.bodyedit.setPlainText(dehtml(body))
+        
+        #self.ui.titleedit.
+
+
+def createConnection():    #数据库操作
+        db = QSqlDatabase.addDatabase("QMYSQL")
+        host = "localhost"
+        port = 3306
+        database = "android"
+        username = "root"
+        password = ""
+        db.setHostName(host)
+        db.setPort(port)
+        db.setDatabaseName(database)
+        db.setUserName(username)
+        db.setPassword(password)
+        if not db.open():    #打印错误信息
+            print("error")
+            QMessageBox.warning(None, u"数据库连接出错", QSqlDatabase.lastError(db).text())
+            print (QSqlDatabase.lastError(db).text()) 
+        else:
+            print ("ok")
+            
+
+
+
+
+
+
 
 class Model(QSqlTableModel):   #数据表操作
     def __init__(self, parent = None):
@@ -94,25 +156,12 @@ class Model(QSqlTableModel):   #数据表操作
         mainWindow.ui.tableView.resizeColumnToContents(0) #tableview列自适应宽度
         mainWindow.ui.tableView.resizeColumnToContents(1) #tableview列自适应宽度
         mainWindow.ui.tableView.resizeColumnToContents(2) #tableview列自适应宽度
-def createConnection():    #数据库操作
-        db = QSqlDatabase.addDatabase("QMYSQL")
-        host = "localhost"
-        port = 3306
-        database = "android"
-        username = "root"
-        password = ""
-        db.setHostName(host)
-        db.setPort(port)
-        db.setDatabaseName(database)
-        db.setUserName(username)
-        db.setPassword(password)
-        if not db.open():    #打印错误信息
-            print("error")
-            QMessageBox.warning(None, u"数据库连接出错", QSqlDatabase.lastError(db).text())
-            print (QSqlDatabase.lastError(db).text()) 
-        else:
-            print ("ok")
-def showdata(self):
+    def search(nid):
+        q = QSqlQuery()
+        #q.exec_("SELECT title FROM t_news WHERE nid = "+nid+";")
+        q.exec_("SELECT title,digest,body FROM t_news WHERE nid = "+nid+";")
+        print ("select"+cid)
+    def showdata(self):
         q = QSqlQuery()
         select = "SELECT * FROM t_news;"
         q.exec_(select)
@@ -128,6 +177,7 @@ if __name__ == '__main__':
     mainWindow.show()    #显示主窗口  
     createConnection()  #连接数据库
     Model()
+    #while True:
+        
     
-
     sys.exit(app.exec_())
